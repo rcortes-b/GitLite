91ab587b0f3fb57d2c106a8cebccbbf78ed68c0a
rcortes-b <raulcortes.dev@gmail.com>
2025-05-26 13:10:43 +0200

    trying argparse, trying python :)

--- GitLite/main.py
+++ GitLite/main.py
@@ -1,5 +1,24 @@
+import sys, argparse
+from messages import *
+
+def init():
+	print('Init thiss siuuu')
+
+def status():
+	print('Status thiss siuuu')
+
 def main():
-	print('Project started and working')
+	if len(sys.argv) < 2:
+		no_args_msg()
+		sys.exit(1)
+	parser = argparse.ArgumentParser(description="A minimal git implementation")
+	commands = parser.add_subparsers(dest="commands", required=True)
+	init_command = commands.add_parser('init')
+	init_command.set_defaults(func=init)
+	status_command = commands.add_parser('status')
+	status_command.set_defaults(func=status)
+	args = parser.parse_args()
+	args.func()
 
 if __name__ == "__main__":
 	main()
---
+++ GitLite/messages.py
@@ -0,0 +1,8 @@
+
+
+def no_args_msg():
+	print('Usage: gitlite <command> <options> <...>\n' \
+	'Commands:\n' \
+	'\tinit\n' \
+	'\thash_object\n'\
+	'\tcommit')
