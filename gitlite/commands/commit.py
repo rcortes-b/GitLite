052b3cff93bae5de872b28657882c34b162479d5
rcortes-b <raulcortes.dev@gmail.com>
2025-07-07 15:59:36 +0200

    check if there are changed to commit

--- gitlite/commands/commit.py
+++ gitlite/commands/commit.py
@@ -10,12 +10,37 @@ from .index import *
 #\n--------------
 #add sha1 index fix and 2/3 status parts done
 
+def no_index_msg():
+	print('On branch master\n')
+	print('Initial commit\n')
+	print("nothing to commit (create/copy files and use \"gitlite add\" to track)")
+	sys.exit(0)
+
+def no_changes_msg():
+	print('On branch master')
+	print('nothing to commit, working tree clean')
+	sys.exit(0)
+
+def check_if_are_changes(prev_commit_sha1, sha1):
+	if '0000000000000000000000000000000000000000' == prev_commit_sha1:
+		return True
+	path = os.path.join(find_gitlite_repo(), 'objects', prev_commit_sha1[:2], prev_commit_sha1[2:])
+	with open(path, 'rb') as f:
+		data = zlib.decompress(f.read())
+	data = data[data.find(b'\x00') +1:].decode()
+	sha = data[5:45]
+	if sha == sha1:
+		return False
+	return True
+
 
 def commit(args):
 	path = find_gitlite_repo(False)
 	if path is None:
 		print('fatal: not gitlite repository found')
 		sys.exit(1)
+	elif not os.path.exists(os.path.join(path, '.gitlite/index')):
+		no_index_msg()
 	a, e = get_author()
 	if a is None and e is None:
 		a = 'default'
@@ -30,7 +55,6 @@ def commit(args):
 	else:
 		with open(path, 'r') as f:
 			prev_commit_sha1 = f.read().replace('\n', '')
-
 	commit_content = []
 	commit_content.append(f"tree {tree_data['sha1']}")
 	commit_content.append(f'parent {prev_commit_sha1}')
@@ -39,12 +63,12 @@ def commit(args):
 	commit_content.append('')
 	commit_content.append(args.message)
 	body = "\n".join(commit_content)
-	print(body)
 	
 	header = f"commit {len(body)}\0"
 	full_data = (header + body).encode()
 	sha1 = hashlib.sha1(full_data).hexdigest()
-
+	if check_if_are_changes(prev_commit_sha1, tree_data['sha1']) is False:
+		no_changes_msg()
 	with open(path, 'w') as f:
 		f.write(f'{sha1}\n')
 	path = find_gitlite_repo()
@@ -56,5 +80,6 @@ def commit(args):
 		os.makedirs(os.path.dirname(path), exist_ok=True)
 	with open(path, 'wb') as f:
 		f.write(zlib.compress(full_data))
+	print('commit has been succesfully created, modifications saved')
 		
 	
--- gitlite/commands/hash_object.py
+++ gitlite/commands/hash_object.py
@@ -2,7 +2,7 @@ from .utils.objects import hash_blob
 
 def hash_object(args):
 	try:
-			sha1 = hash_blob(args.file, args.write)
-			print(sha1)
+		sha1 = hash_blob(args.file, args.write)
+		print(sha1)
 	except FileNotFoundError:
 		print('fatal: file couldn\'t be opened')
