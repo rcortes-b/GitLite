042987b99e8c51ca87be81e75b8c8781cead48aa
rcortes-b <raulcortes.dev@gmail.com>
2025-07-19 12:17:02 +0200

    status probably done --- need hard testing

--- gitlite/commands/status.py
+++ gitlite/commands/status.py
@@ -7,16 +7,25 @@ from .index import read_index
 # index and work tree
 # index and commit
 
-def message_check(message_trigger):
+def message_check(message_trigger, mode=0):
 	if message_trigger is False:
-		print('Changes to be committed:')
+		if mode == 0:
+			print('Changes to be committed:')
+			print('  (use "git rm --cached <file>..." to unstage)')
+		elif mode == 1:
+			print('\nChanges not staged for commit:')
+			print('  (use "git add <file>..." to update what will be committed)')
+		else:
+			print('\nUntracked files:')
+			print('  (use "git add <file>..." to include in what will be committed)')
 	return True
 
 def changes_to_commit(entries=None, all_files=None, index_paths=None):
+	message_trigger = False
 	if entries is None:
 		print('No commits yet\n')
+		message_trigger = True
 	else:
-		message_trigger = False
 		commit_files = get_commit_files_v2()
 		if commit_files is None:
 			print('No commits yet\n')
@@ -41,6 +50,38 @@ def changes_to_commit(entries=None, all_files=None, index_paths=None):
 								message_trigger = message_check(message_trigger)
 								print('\t', f"\033[92m modified:\t{entry['path']}\033[0m")
 							commit_files.remove(files)
+	return message_trigger
+
+def changes_to_add(entries=None, all_files=None, path=None):
+	message_trigger = False
+	if entries is None:
+		return message_trigger
+	elif all_files is None:
+		message_trigger = message_check(message_trigger, mode=1)
+		for entry in entries:
+			print('\t', f"\033[91m deleted:\t{entry['path']}\033[0m")
+	else:
+		for entry in entries:
+			if file_in_list(all_files, entry['path']) is True:
+				if entry['fields']['sha1'] != hash_blob(os.path.join(path, entry['path']), False):
+					message_trigger = message_check(message_trigger, mode=1)
+					print('\t', f"\033[91m modified:\t{entry['path']}\033[0m")
+	return message_trigger
+
+def untracked_files(all_files=None, path_index_entries=None):
+	message_trigger = False
+	if path_index_entries is None:
+		if all_files is None:
+			return message_trigger
+		for file in all_files:
+			message_trigger = message_check(message_trigger, mode=2)
+			print('\t', f"\033[91m{file}")
+	else:
+		for file in all_files:
+			if file_in_list(path_index_entries, file) is False:
+				message_trigger = message_check(message_trigger, mode=2)
+				print('\t', f"\033[91m{file}")
+	return message_trigger
 
 def status(args):
 	path = find_gitlite_repo(False)
@@ -56,97 +97,8 @@ def status(args):
 		status_default_msg()
 		sys.exit(0)
 	print('On branch master')
-	changes_to_commit(index_entries, all_files, path_index_entries)
-	sys.exit(0)
-	if index_entries is not None:
-		print('Your branch is up to date with \'origin/main\'.\n')
-		print('Changes to be committed:')
-		#index and commit
-		commit_files = get_commit_files()
-		if commit_files is not None:
-			for index in index_entries:
-				found = False
-				for commit in commit_files:
-					if index['path'] == commit['path']:
-						if index['fields']['sha1'] != commit['sha']:
-							print('\t', f"\033[92m modified:\t{index['path']}\033[0m")
-						commit_files.remove(commit)
-						found = True
-						break
-				if found is False:
-					print('\t', f"\033[92m new file:\t{index['path']}\033[0m")
-			if len(commit_files) > 0:
-				for removed_file in commit_files:
-					print('\t', f"\033[91m removed:\t{removed_file['path']}\033[0m")
-		print('\nChanges not staged for commit:')
-		#index and work tree
-		for entry in index_entries:
-			if file_in_list(all_files, entry['path']) is True:
-				if entry['fields']['sha1'] != hash_blob(os.path.join(path, entry['path']), False):
-					print('\t', f"\033[91m modified:\t{entry['path']}\033[0m")
-			else:
-				print('\t', f"\033[91m deleted:\t{entry['path']}\033[0m")
-	else:
-		print('On branch master\n\nNo commits yet')
-	if all_files is not None:
-		print('\nUntracked files:')
-		#print(index_entries)
-		for file in all_files:
-			if file_in_list(path_index_entries, file) is False:
-				print('\t', f"\033[91m{file}")
-
-
-
-
-
-#def status(args):
-#	path = find_gitlite_repo(False)
-#	if path is None:
-#		print('fatal: not gitlite repository found')
-#		sys.exit(1)
-#	index_entries = read_index()
-#	path_index_entries = None
-#	if index_entries is not None:
-#		path_index_entries = [entry['path'] for entry in index_entries]
-#	ignored_files = get_ignored_files()
-#	all_files = get_all_files()
-#	if all_files is None and index_entries is None:
-#		status_default_msg()
-#		sys.exit(0)
-#	if index_entries is not None:
-#		print('On branch master')
-#		print('Your branch is up to date with \'origin/main\'.\n')
-#		print('Changes to be committed:')
-#		#index and commit
-#		commit_files = get_commit_files()
-#		if commit_files is not None:
-#			for index in index_entries:
-#				found = False
-#				for commit in commit_files:
-#					if index['path'] == commit['path']:
-#						if index['fields']['sha1'] != commit['sha']:
-#							print('\t', f"\033[92m modified:\t{index['path']}\033[0m")
-#						commit_files.remove(commit)
-#						found = True
-#						break
-#				if found is False:
-#					print('\t', f"\033[92m new file:\t{index['path']}\033[0m")
-#			if len(commit_files) > 0:
-#				for removed_file in commit_files:
-#					print('\t', f"\033[91m removed:\t{removed_file['path']}\033[0m")
-#		print('\nChanges not staged for commit:')
-#		#index and work tree
-#		for entry in index_entries:
-#			if file_in_list(all_files, entry['path']) is True:
-#				if entry['fields']['sha1'] != hash_blob(os.path.join(path, entry['path']), False):
-#					print('\t', f"\033[91m modified:\t{entry['path']}\033[0m")
-#			else:
-#				print('\t', f"\033[91m deleted:\t{entry['path']}\033[0m")
-#	else:
-#		print('On branch master\n\nNo commits yet')
-#	if all_files is not None:
-#		print('\nUntracked files:')
-#		#print(index_entries)
-#		for file in all_files:
-#			if file_in_list(path_index_entries, file) is False:
-#				print('\t', f"\033[91m{file}")
+	trigger_commit = changes_to_commit(index_entries, all_files, path_index_entries)
+	trigger_add = changes_to_add(index_entries, all_files, path)
+	trigger_untracked = untracked_files(all_files, path_index_entries)
+	if not (trigger_commit or trigger_add or trigger_untracked):
+		print('nothing to commit, working tree clean')
